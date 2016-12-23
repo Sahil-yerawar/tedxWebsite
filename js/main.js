395810195807bab26738f0161064eeb398ad995b
Chait97 <chaitwheels@gmail.com>
2016-12-24 03:14:22 +0530

    Fontend makeover for the website. Added tags for SEO. Added Facebook Plugin, Added favicon

--- js/main.js
+++ js/main.js
@@ -1,41 +1,28 @@
 jQuery(function($) {'use strict',
-	
+
 	//Countdown js
 	 $("#countdown").countdown({
 			date: "10 july 2017 12:00:00",
 			format: "on"
 		},
-		
+
 		function() {
 			// callback function
 		});
-	
 
-	
+
+
 	//Scroll Menu
 
-	function menuToggle()
-	{
+	function menuToggle(){
 		var windowWidth = $(window).width();
-
-		if(windowWidth > 767 ){
-			$(window).on('scroll', function(){
-				if( $(window).scrollTop()>405 ){
-					$('.main-nav').addClass('fixed-menu animated slideInDown');
-				} else {
-					$('.main-nav').removeClass('fixed-menu animated slideInDown');
-				}
-			});
-		}else{
-			
-			$('.main-nav').addClass('fixed-menu animated slideInDown');
-				
-		}
+		$('.main-nav').addClass('fixed-menu animated slideInDown');
+		console.log("added");
 	}
 
 	menuToggle();
-	
-	
+
+
 	// Carousel Auto Slide Off
 	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
 		interval: false
@@ -66,85 +53,3 @@ jQuery(function($) {'use strict',
 	});
 
 });
-
-
-// Google Map Customization
-(function(){
-
-	var map;
-
-	map = new GMaps({
-		el: '#gmap',
-		lat: 43.04446,
-		lng: -76.130791,
-		scrollwheel:false,
-		zoom: 16,
-		zoomControl : false,
-		panControl : false,
-		streetViewControl : false,
-		mapTypeControl: false,
-		overviewMapControl: false,
-		clickable: false
-	});
-
-	var image = 'images/map-icon.png';
-	map.addMarker({
-		lat: 43.04446,
-		lng: -76.130791,
-		icon: image,
-		animation: google.maps.Animation.DROP,
-		verticalAlign: 'bottom',
-		horizontalAlign: 'center',
-		backgroundColor: '#3e8bff',
-	});
-
-
-	var styles = [ 
-
-	{
-		"featureType": "road",
-		"stylers": [
-		{ "color": "#b4b4b4" }
-		]
-	},{
-		"featureType": "water",
-		"stylers": [
-		{ "color": "#d8d8d8" }
-		]
-	},{
-		"featureType": "landscape",
-		"stylers": [
-		{ "color": "#f1f1f1" }
-		]
-	},{
-		"elementType": "labels.text.fill",
-		"stylers": [
-		{ "color": "#000000" }
-		]
-	},{
-		"featureType": "poi",
-		"stylers": [
-		{ "color": "#d9d9d9" }
-		]
-	},{
-		"elementType": "labels.text",
-		"stylers": [
-		{ "saturation": 1 },
-		{ "weight": 0.1 },
-		{ "color": "#000000" }
-		]
-	}
-
-	];
-
-	map.addStyle({
-		styledMapName:"Styled Map",
-		styles: styles,
-		mapTypeId: "map_style"  
-	});
-
-	map.setStyle("map_style");
-}());
-
-
-
